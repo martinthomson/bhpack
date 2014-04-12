930ff0cfe54bd54437311351108185f55b143897
Martin Thomson <martin.thomson@gmail.com>
2014-04-12 10:53:23 -0700

    Formatting tweaks

--- compare.js
+++ compare.js
@@ -5,35 +5,48 @@ var bhpack = require('./bhpack');
 var hpack = require('./hpack');
 var base64 = require('urlsafe-base64');
 
+function Score() {
+  this.len =  0;
+  this.max = 0;
+  this.min = Infinity;
+}
+Score.prototype = {
+  add: function(d) {
+    var l = d.length;
+    this.len += l;
+    if (l > this.max) {
+      this.max = l;
+    }
+    if (l < this.min) {
+      this.min = l;
+    }
+  },
+  average: function(rounds) {
+    return this.len / rounds;
+  }
+};
 function compare(rounds, size) {
 //  var rawSize = 0;
-  var hpackLen = 0;
-  var base64Len = 0;
-  var base64HpackLen = 0;
-  var bhpackCookieHpackLen = 0;
-  var bhpackUriSafeHpackLen = 0;
-  var bhpackUriQueryHpackLen = 0;
+  var score = {};
+  [
+    'hpack', 'base64', 'base64Hpack', 'bhpackCookie', 'bhpackUri', 'bhpackQuery'
+  ].forEach(function(k) {
+    score[k] = new Score();
+  });
   for (var i = 0; i < rounds; ++i) {
     var data = crypto.randomBytes(size);
-    hpackLen += hpack.encode(data).length;
+    score.hpack.add(hpack.encode(data));
     var b64 = new Buffer(base64.encode(data), 'utf8');
-    base64Len += b64.length;
-    base64HpackLen += hpack.encode(b64).length;
+    score.base64.add(b64);
+    score.base64Hpack.add(hpack.encode(b64));
     var cookie = new Buffer(bhpack.cookie.encode(data), 'utf8');
-    bhpackCookieHpackLen += hpack.encode(cookie).length;
+    score.bhpackCookie.add(hpack.encode(cookie));
     var uri = new Buffer(bhpack.uriSafe.encode(data), 'utf8');
-    bhpackUriSafeHpackLen += hpack.encode(uri).length;
+    score.bhpackUri.add(hpack.encode(uri));
     var query = new Buffer(bhpack.uriQuery.encode(data), 'utf8');
-    bhpackUriQueryHpackLen += hpack.encode(query).length;
+    score.bhpackQuery.add(hpack.encode(query));
   }
-  return {
-    hpack: hpackLen,
-    base64: base64Len,
-    base64Hpack: base64HpackLen,
-    bhpackCookie: bhpackCookieHpackLen,
-    bhpackUri: bhpackUriSafeHpackLen,
-    bhpackQuery: bhpackUriQueryHpackLen
-  };
+  return score;
 }
 
 var argv = process.argv.slice(2);
@@ -47,7 +60,7 @@ var rounds = parseInt(argv.shift(), 10);
 var sizes = argv.map(function(size) {
   return parseInt(size, 10);
 });
-console.log(rounds + ' random sequences of lengths: \t' + sizes.join('\t\t'));
+console.log(rounds + ' random sequences of lengths: \t' + sizes.join('\t\t\t'));
 var results = sizes.map(function(size) {
   return compare(rounds, size);
 });
@@ -57,11 +70,12 @@ var referenceKey = 'base64Hpack';
 function printResults(label, key, pad) {
   pad = (typeof pad !== 'undefined') ? pad : '';
   console.log(label + ': \t\t' + pad + results.map(function(r) {
-    var average = r[key] / rounds;
-    var improvement = Math.round(r[key] / r[referenceKey] * 10000) / 100;
-    return average + ' (' + improvement + ')';
+    var stats = r[key].min + '/' + r[key].average(rounds) + '/' + r[key].max;
+    var improvement = Math.round(r[key].len / r[referenceKey].len * 10000) / 100;
+    return stats + ' (' + improvement + ')';
   }).join('\t'))
 }
+//console.log(results);
 printResults('Raw Huffman coding (invalid)', 'hpack');
 printResults('Base 64 (no Huffman)', 'base64', '\t');
 printResults('Base 64 with Huffman', 'base64Hpack', '\t');
